afa9dcda498b36ca3f6cc96e63ca59bc36b04d35
Ben Belcourt <ben.belcourt@gmail.com>
2013-04-12 13:56:37 -0400

    added more unit tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,5 +1,14 @@
 module('DOM methods');
-test("createNode test",4, function () {
+test('Basic Requirements',6, function () {
+	ok(document.getElementById, 'getElementByID');
+	ok(document.querySelectorAll, 'querySelectorAll');
+	ok(document.getElementsByTagName, 'getElementsByTagName');
+	ok(Array.prototype.push, 'Array.push()');
+	ok(document.createElement, 'createElement');
+	ok(Element.prototype.setAttribute, 'Element.setAttribute');
+});
+
+test('createNode',4, function () {
 	var node = BB.lib.createNode({
 		'type': 'div',
 		'innerHTML': 'this is div text',
@@ -15,14 +24,13 @@ test("createNode test",4, function () {
 	equal(node.className, 'test-class', 'Correct class applied');
 });
 
-test("getElementByClass", function () {
+test('getElementByClass', function () {
 	var _fixture = document.getElementById('qunit-fixture');
 	var _node = document.createElement('a');
 	_node.setAttribute('class', 'test-link');
 	_fixture.appendChild(_node);
 	
 	var _link = BB.lib.getElementsByClass('test-link', _fixture);
-	console.log(_link[0]);
 
 	equal(_link.length, 1, 'Correct number of elements returned');
 	equal(_link[0].className, 'test-link', 'Correct class applied');
